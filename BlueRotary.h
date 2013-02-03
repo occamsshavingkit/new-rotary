4355aa23db5ef682712e2ae5da03c2445fb1871c
occamsshavingkit <occamsshavingkit@gmail.com>
2013-02-02 17:45:56 -0800

    First upload

---
+++ BlueRotary.h
@@ -0,0 +1,323 @@
+//*******************************************************
+//					GPIO Definitions
+//*******************************************************
+#define HOOK		0		//Port C.0
+#define BT_EN		1		//Port C.1
+#define RING_PWR	2		//Port C.2
+#define RING1		3		//Port C.3
+#define RING2		4		//Port C.4
+#define PSTAT		5		//Port C.5
+#define BATT		7
+
+#define BT_TX		0		//Port D.0
+#define BT_RX		1		//Port D.1
+#define	BT_RES		2		//Port D.2
+#define EROTARY		3		//Port D.3
+#define ROTARY		4		//Port D.4
+#define BT_UNPAIR   5       //Port D.5
+#define DT1			6		//Port D.6
+#define DT2			7		//Port D.7
+
+#define	CS			2		//Port B.2
+#define MOSI		3		//Port B.3
+#define MISO		4		//Port B.4
+#define SCK			5		//Port B.5
+
+//*******************************************************
+//						Macros
+//*******************************************************
+#define	LED_ON()	cbi(PORTC, PSTAT);
+#define	LED_OFF()	sbi(PORTC, PSTAT);
+
+#define sbi(var, mask)   ((var) |= (uint8_t)(1 << mask))
+#define cbi(var, mask)   ((var) &= (uint8_t)~(1 << mask))
+//*******************************************************
+//					General Definitions
+//*******************************************************
+#define BAUD_RATE 57600U
+#define MAX_MESSAGE_LENGTH	128	//Buffer length for UART messages
+#define MAX_NUMBER_LENGTH 20
+#define	OK		1
+#define	ERROR	0
+#define	OFF_HOOK	1
+#define	ON_HOOK		0
+#define	ROTARY_DIALING	1
+#define	ROTARY_FINISHED	0
+#define STEP_SHIFT 7
+#define SINE_SAMPLES 255U
+#define TICKS_PER_CYCLE 256U
+// see http://www.atmel.com/Images/doc1982.pdf
+#define STEP_350 ((unsigned)(350UL * SINE_SAMPLES * (TICKS_PER_CYCLE << STEP_SHIFT) / F_CPU))
+#define STEP_440 ((unsigned)(440UL * SINE_SAMPLES * (TICKS_PER_CYCLE << STEP_SHIFT) / F_CPU))
+
+//=======================================================
+//					Function Definitions
+//=======================================================
+int uart_getchar(FILE *);
+void ioinit(void);
+void delay_ms(uint16_t x);
+void config_bluetooth(void);
+void incoming_call(void);
+void place_call(void);		
+void ring_it(void);			
+void short_ring_it(void);	
+char string_compare(const char *search_string, const char *find_string);	
+void dial_tone(void);	
+char get_rotary_number(void);
+void dial_number(void);	
+
+const unsigned char sine_table[] PROGMEM = {128,
+    131,
+    134,
+    137,
+    140,
+    143,
+    146,
+    149,
+    152,
+    156,
+    159,
+    162,
+    165,
+    168,
+    171,
+    174,
+    176,
+    179,
+    182,
+    185,
+    188,
+    191,
+    193,
+    196,
+    199,
+    201,
+    204,
+    206,
+    209,
+    211,
+    213,
+    216,
+    218,
+    220,
+    222,
+    224,
+    226,
+    228,
+    230,
+    232,
+    234,
+    235,
+    237,
+    239,
+    240,
+    242,
+    243,
+    244,
+    246,
+    247,
+    248,
+    249,
+    250,
+    251,
+    251,
+    252,
+    253,
+    253,
+    254,
+    254,
+    254,
+    255,
+    255,
+    255,
+    255,
+    255,
+    255,
+    255,
+    254,
+    254,
+    253,
+    253,
+    252,
+    252,
+    251,
+    250,
+    249,
+    248,
+    247,
+    246,
+    245,
+    244,
+    242,
+    241,
+    239,
+    238,
+    236,
+    235,
+    233,
+    231,
+    229,
+    227,
+    225,
+    223,
+    221,
+    219,
+    217,
+    215,
+    212,
+    210,
+    207,
+    205,
+    202,
+    200,
+    197,
+    195,
+    192,
+    189,
+    186,
+    184,
+    181,
+    178,
+    175,
+    172,
+    169,
+    166,
+    163,
+    160,
+    157,
+    154,
+    151,
+    148,
+    145,
+    142,
+    138,
+    135,
+    132,
+    129,
+    126,
+    123,
+    120,
+    117,
+    113,
+    110,
+    107,
+    104,
+    101,
+    98,
+    95,
+    92,
+    89,
+    86,
+    83,
+    80,
+    77,
+    74,
+    71,
+    69,
+    66,
+    63,
+    60,
+    58,
+    55,
+    53,
+    50,
+    48,
+    45,
+    43,
+    40,
+    38,
+    36,
+    34,
+    32,
+    30,
+    28,
+    26,
+    24,
+    22,
+    20,
+    19,
+    17,
+    16,
+    14,
+    13,
+    11,
+    10,
+    9,
+    8,
+    7,
+    6,
+    5,
+    4,
+    3,
+    3,
+    2,
+    2,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    2,
+    2,
+    3,
+    4,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    11,
+    12,
+    13,
+    15,
+    16,
+    18,
+    20,
+    21,
+    23,
+    25,
+    27,
+    29,
+    31,
+    33,
+    35,
+    37,
+    39,
+    42,
+    44,
+    46,
+    49,
+    51,
+    54,
+    56,
+    59,
+    62,
+    64,
+    67,
+    70,
+    73,
+    76,
+    79,
+    81,
+    84,
+    87,
+    90,
+    93,
+    96,
+    99,
+    103,
+    106,
+    109,
+    112,
+    115,
+    118,
+    121,
+    124};
---
+++ Blue_Rotary-v3.c
@@ -0,0 +1,182 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/pgmspace.h>
+#include "uart.h"
+#include "BlueRotary.h"
+
+
+//================================================================
+//Define Global Variables
+//================================================================
+volatile char message[MAX_MESSAGE_LENGTH];	//Buffer for UART messages
+volatile char message_complete, ring_tone_flag;	//general purpse flags
+char final_message[MAX_MESSAGE_LENGTH];	//Final buffer for UART messages
+volatile int message_index=0;
+int dialed_number, counter;
+int get_number_timeout=0;
+char number_length, temp;
+char phone_number[MAX_NUMBER_LENGTH];
+char rotary_high;
+int connected=0;
+unsigned int location_350=0, location_440=0;
+
+ISR(TIMER0_OVF_vect)
+{
+	cli();
+    sleep_disable();
+    sbi(PORTD,DT1);
+    sbi(PORTD,DT2);
+	sei();
+    sleep_enable();
+}
+
+ISR(TIMER2_COMPA_vect) // 350 Hz buzz
+{
+    cli();
+    sleep_disable();
+    cbi(PORTD,DT1);
+    OCR2A = pgm_read_byte(&(sine_table[location_350 >> STEP_SHIFT]));
+    location_350 += STEP_350;
+    if(location_350 >= (SINE_SAMPLES << STEP_SHIFT)) location_350 -= (SINE_SAMPLES << STEP_SHIFT);
+    sei();    
+    sleep_enable();
+}
+
+ISR(TIMER2_COMPB_vect) // 440 Hz buzz
+{
+    cli();
+    sleep_disable();
+    cbi(PORTD,DT2);
+    OCR2B = pgm_read_byte(&(sine_table[location_440 >> STEP_SHIFT]));
+    location_440 += STEP_440;
+    if(location_440 >= (SINE_SAMPLES << STEP_SHIFT)) location_440 -= (SINE_SAMPLES << STEP_SHIFT);
+    sei();
+    sleep_enable();
+}
+
+ISR(PCINT1_vect) //HOOK
+{
+    cli();
+    sleep_disable();
+    _delay_ms(50);
+    if(PINC & (1<<HOOK)) { // picked up!
+        LED_OFF();
+        TCCR0B |= (1<<CS00); // turn on dial tone
+    }
+    else { // hung up!
+        TCCR0B &= ~(1<<CS00); // turn off dial tone
+        counter = 0; // clear out the dialed number
+        LED_ON();
+    }
+    sei();
+    sleep_enable();
+}
+
+ISR(PCINT2_vect) // ROTARY (or EROTARY)
+{
+    cli();
+    if(PIND & (1<<ROTARY)){
+        rotary_high = 1;
+        _delay_ms(20);
+    }
+    if ((PIND & (1<<ROTARY)) != (1 << ROTARY)){
+        _delay_ms(20);
+        TCCR0B &= ~(1<<CS00); // turn off dial tone
+        if(rotary_high == 1){
+            rotary_high = 0;
+            dialed_number++;
+        }
+    }
+    if (PIND & (1<<EROTARY)){
+        dialed_number--;
+        if(counter < MAX_NUMBER_LENGTH){
+        phone_number[counter++] = dialed_number;
+        }
+        else {
+            // error_ring();
+        }
+        for(char i = 0; i < dialed_number; i++){
+            LED_ON();
+            _delay_ms(200);
+            LED_OFF();
+            _delay_ms(200);
+        }
+        dialed_number = 0;
+    }
+    PCMSK2 &= ~((1<<PCINT19)|(1<<PCINT20));
+    sei();
+    PCMSK2 |= (1<<PCINT19)|(1<<PCINT20);
+}
+
+//Function: ioinit
+//Purpose:	Initialize AVR I/O, UART and Interrupts
+//Inputs:	None
+//Outputs:	None
+void ioinit(void)
+{
+    //1 = output, 0 = input
+	DDRB = (1<<CS) | (1<<SCK) | (1<<MOSI);										//Define Port B GPIO Outputs
+    PORTB = (1<<MISO);
+	DDRB &= ~(1<<MISO);
+    
+	DDRC = (1<<BT_EN) | (1<<RING_PWR) | (1<<RING1) | (1<<RING2) |  (1<<PSTAT); 	//Define Port C Outputs
+	PORTC = (1<<HOOK);
+	DDRC &= ~(1<<HOOK);															//Define Port C Inputs
+    
+	DDRD = (1<<DT1) | (1<<DT2) | (1<< BT_RES);										//Define Port D Outputs
+	PORTD = (1<<ROTARY) | (1<<EROTARY);	
+    DDRD &= ~((1<<ROTARY)|(1<<EROTARY));								//Define Port D inputs
+    
+    //SPI Bus setup
+	//SPCR = (1<<SPE)|(1<<MSTR)|(1<<CPHA)|(1<<CPOL)|(1<<SPR0);	//Might need to change the phase
+
+    uart_init(BAUD_RATE);
+    
+    cli();
+    
+    
+	//Init timer for dial tone
+    ASSR = 0;
+    TCCR2A=(1<<WGM21)|(1<<WGM20);
+    //    TCCR0B=(1<<CS00); // this turns on the timer now!
+    TIMSK2 = (1<<OCIE2A)|(1<<TOIE2)|(1<<OCIE2B);
+	OCR2A=(0x00);		//Load Compare Register with Delay
+    OCR2B=(0x00);
+    PCICR |= (1<<PCIE0)|(1<<PCIE1)|(1<<PCIE2);
+    PCMSK1 |= (1<<PCINT8);
+    PCMSK2 |= (1<<PCINT19)|(1<<PCINT20);
+}
+
+int main(void){
+    int uart_recv;
+    char uart_err;
+    char uart_char;
+    ioinit();
+    sleep_enable();
+    set_sleep_mode(SLEEP_MODE_IDLE);
+    sei();
+    message_index = 0;
+    while(1){
+        uart_recv = uart_getc();
+        uart_err = (c & 0xff00) >> 8;
+        uart_char = c & 0x00ff;
+        if(!(uart_err) && message_index < MAX_MESSAGE_LENGTH)
+        {
+            message[message_index++] = uart_char;
+        }
+        if(uart_char == '\n')
+        {
+            message[message_index-1]='\0';
+            message_index = 0;
+            interpret_message(message);
+        } 
+        sleep_cpu();        
+    }
+    return 0;
+}
